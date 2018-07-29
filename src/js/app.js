918aa99a5a5c7aed39c526626b39e2929010d130
Cissy <1395422644@qq.com>
2018-07-29 21:44:17 +0800

    注册和登录，保存用户信息

--- src/js/app.js
+++ src/js/app.js
@@ -12,25 +12,63 @@ var app = new Vue({
             email:"邮箱",
             phone:"手机"
         },
-
+        login:{
+            email:'',
+            password:''
+        },
+        register:{
+            email:'',
+            password:''
+        }
     },
     methods:{
         y(key,value){
             this.resume[key]=value
         },
+        onLogin(e){
+            AV.User.logIn(this.login.email, this.login.password).then(function (loggedInUser) {
+                console.log(loggedInUser);
+              }, function (error) {
+                  if(error.code===211){
+                      alert("该邮箱没有被注册")
+                  }else if (error.code===210){
+                      alert('密码与邮箱不匹配')
+                  }
+              })
+        },
+        onRegister(e){
+            var user = new AV.User();
+            // 设置用户名
+            user.setUsername(this.register.email);
+            // 设置密码
+            user.setPassword(this.register.password);
+            // 设置邮箱
+            user.setEmail(this.register.email);
+            user.signUp().then(function (loggedInUser) {
+            }, function (error) {
+                console.log(error)
+            })
+        },
         handleSave(){
             var currentUser = AV.User.current();
             if (currentUser) {
-               this.saveResume()
-            }
-            else {
+                this.saveResume()
+            }else {
                this.showLogin()
             }
         },
         showLogin(){
             this.loginVisible=true;
         },
-        saveResume(){},
+        saveResume(){
+           
+            let id=AV.User.current().id;
+            var user = AV.Object.createWithoutData('User', id);
+            // 修改属性
+            user.set('resume', this.resume);
+            // 保存到云端
+            user.save();
+        },
 
     },
    
